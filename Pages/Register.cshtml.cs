0b0499a80a807a4046e7632dddc55c7c51b12e7a
Brian Akbar <brianakbar@yahoo.com.sg>
2023-10-07 03:55:09 +0700

    Add detailed register page

--- Pages/Register.cshtml.cs
+++ Pages/Register.cshtml.cs
@@ -5,11 +5,17 @@ namespace ACorp.Pages;
 
 public class RegisterModel : PageModel
 {
+    public string? Email { get; set; } = null;
+
     private readonly ILogger<RegisterModel> _logger;
 
     public RegisterModel(ILogger<RegisterModel> logger)
     {
         _logger = logger;
+    }
+
+    public void OnPostEmail(string? inputEmail) {
+        Email = inputEmail;
     }
 
     public void OnGet()
