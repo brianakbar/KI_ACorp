80294c5f2b7f67ffc8a61b17a9969ed428a19629
Brian Akbar <brianakbar@yahoo.com.sg>
2023-10-08 03:18:38 +0700

    Add dashboard account page

---
+++ Pages/Dashboard/File.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ACorp.Pages.Dashboard;
+
+public class FileModel : PageModel
+{
+    private readonly ILogger<FileModel> _logger;
+
+    public FileModel(ILogger<FileModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
