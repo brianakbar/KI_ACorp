62872056829b666e6fe2e831810863605ddf628f
Brian Akbar <brianakbar@yahoo.com.sg>
2023-10-17 08:21:48 +0700

    Update upload file

--- Pages/Dashboard/File.cshtml.cs
+++ Pages/Dashboard/File.cshtml.cs
@@ -1,7 +1,10 @@
+using System.Security.Claims;
+using ACorp.Authentication;
+using KiAcorp.Data;
+using KiAcorp.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 
 namespace ACorp.Pages.Dashboard;
 
@@ -9,25 +12,66 @@ namespace ACorp.Pages.Dashboard;
 public class FileModel : PageModel
 {
     private readonly IWebHostEnvironment _environment;
+    private readonly ApplicationDbContext _db;
+    private readonly AuthService _authService;
 
-    public FileModel(IWebHostEnvironment environment)
+    public FileModel(ApplicationDbContext db, IWebHostEnvironment environment)
     {
         _environment = environment;
-
+        _db = db;
+        _authService = new(_db);
     }
 
     [BindProperty]
-    public IFormFile Upload { get; set; }
+    public IFormFile UploadKTP { get; set; }
+
+    [BindProperty]
+    public IFormFile UploadCV { get; set; }
+
+    [BindProperty]
+    public IFormFile UploadVideo { get; set; }
 
     public void OnGet()
     {
 
     }
 
-    public async Task OnPostAsync()
+    public async Task OnPostKtpAsync()
     {
-        var file = Path.Combine(_environment.ContentRootPath, "Storage", Upload.FileName);
-        using var fileStream = new FileStream(file, FileMode.Create);
-        await Upload.CopyToAsync(fileStream);
+        User user = await GetUser() ?? throw new InvalidOperationException("User is not found.");
+        await UploadFile(UploadKTP, user.Id + "_" + "KTP" + Path.GetExtension(UploadKTP.FileName));
+    }
+
+    public async Task OnPostCvAsync()
+    {
+        User user = await GetUser() ?? throw new InvalidOperationException("User is not found.");
+        await UploadFile(UploadCV, user.Id + "_" + "CV" + Path.GetExtension(UploadCV.FileName));
+    }
+
+    public async Task OnPostVideoAsync()
+    {
+        User user = await GetUser() ?? throw new InvalidOperationException("User is not found.");
+        await UploadFile(UploadVideo, user.Id + "_" + "Video" + Path.GetExtension(UploadVideo.FileName));
+    }
+
+    private async Task UploadFile(IFormFile file, string name)
+    {
+        string folderPath = Path.Combine(_environment.ContentRootPath, "Storage");
+        Directory.CreateDirectory(folderPath);
+        string oldFilePath = Path.Combine(folderPath, file.FileName);
+        string newFilePath = Path.Combine(folderPath, name);
+        using (var fileStream = new FileStream(oldFilePath, FileMode.Create))
+        {
+            await file.CopyToAsync(fileStream);
+        }
+        System.IO.File.Move(oldFilePath, newFilePath);
+    }
+
+    private async Task<User?> GetUser()
+    {
+        ClaimsIdentity? claimsIdentity = User.Identity as ClaimsIdentity;
+        string email = (claimsIdentity?.FindFirst(ClaimTypes.Email)?.Value) ?? throw new InvalidOperationException("User email is not found.");
+
+        return await _authService.FindUserAsync(email);
     }
 }
